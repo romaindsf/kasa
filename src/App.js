b17f8e276e4dfb1c88fca5b214268a9cd5ee15cf
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-03-07 00:46:26 +0100

    added <Footer /> and it's own style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 
 function App() {
-  return (
+  return ( 
+  <>
     <Header />
+    <Footer />
+  </>
   );
 }
 
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import logo_footer from "../../assets/logo_footer.svg"
+
+function Footer () {
+    return (
+        <footer>
+            <img src={logo_footer} alt="logo Kasa blanc sur fond noir" />
+            <p>&copy; 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
