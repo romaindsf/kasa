1781f625a301d70ef49afe57d43d3dbc678d6c90
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-03-12 20:21:30 +0100

    rental_list & currentIndex are now props to the slideshow

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -1,20 +1,15 @@
-import { useParams } from 'react-router-dom';
 import React, { useState } from 'react';
-import rental_list from "../../datas/rental_ad_list.json"
 
-function Slideshow() {
-    const { rentalId } = useParams();
+function Slideshow({rental_list, currentIndex}) {
     const [currentSlide, setCurrentSlide] = useState(0);
-
-    const currentIndex = rental_list.findIndex(rental => rental.id === rentalId);
 
     const nextSlide = () => {
         setCurrentSlide((currentSlideIndex) => (currentSlideIndex === rental_list[currentIndex].pictures.length - 1 ? 0 : currentSlideIndex + 1));
-      };
+    };
     
-      const prevSlide = () => {
+    const prevSlide = () => {
         setCurrentSlide((currentSlideIndex) => (currentSlideIndex === 0 ? rental_list[currentIndex].pictures.length - 1 : currentSlideIndex - 1));
-      };
+    };
 
     return (
         <div className="slideshow">
--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,13 +1,16 @@
 import { useParams } from 'react-router-dom'
-import RentalList from '../../components/rental_list/RentalList'
+import rental_list from '../../datas/rental_ad_list.json'
 import Slideshow from '../../components/slideshow/Slideshow'
 
 function Housing() {
     const {rentalId} = useParams()
+    const currentIndex = rental_list.findIndex(rental => rental.id === rentalId);
 
     return (
         <div className='main_container'>
-            <Slideshow />
+            <Slideshow 
+                rental_list={rental_list} 
+                currentIndex={currentIndex}/>
         </div>
     )
 }
