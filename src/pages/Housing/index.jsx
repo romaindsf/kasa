c9c1b498d94cefcc7f0cc2566d6668537f43b6f7
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-03-13 11:55:28 +0100

    data is now read from the rental array and not the entire data file

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -5,28 +5,27 @@ import Tags from '../../components/tags/Tags'
 
 function Housing() {
     const {rentalId} = useParams()
-    const currentIndex = rental_list.findIndex(rental => rental.id === rentalId);
+    const rentalInfo = rental_list.find(rental =>rental.id === rentalId)
 
     return (
         <div className='main_container rental_info'>
-            <Slideshow 
-                rental_list={rental_list} 
-                currentIndex={currentIndex}
-            />
-            <h1>{rental_list[currentIndex].title}</h1>
-            <p>{rental_list[currentIndex].location}</p>
-            <Tags 
-                rental_list={rental_list}
-                currentIndex={currentIndex}
-            />
-            <div className='rating_host'>
-                <p>{rental_list[currentIndex].host.name}</p>
-                <img src={rental_list[currentIndex].host.picture} 
-                    alt={`Profil de ${rental_list[currentIndex].host.name}`}
-                />
+            <div>
+                <Slideshow rentalInfo={rentalInfo}/>
+                <h1>{rentalInfo.title}</h1>
+                <p>{rentalInfo.location}</p>
+                <Tags rentalInfo={rentalInfo}/>
             </div>
-            <div className='description_equipments'>
-                {/* component Description et Equipment  */}
+            <div className='ratingHost'>
+                {/* rating */}
+                <div className='ratingHost__host'>
+                    <p>{rentalInfo.host.name}</p>
+                    <img src={rentalInfo.host.picture} 
+                        alt={`Profil de ${rentalInfo.host.name}`}
+                    />
+                </div>
+            </div>
+            <div>
+                {/* description et equipment */}
             </div>
         </div>
     )
