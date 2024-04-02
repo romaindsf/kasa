0d79321c3a4092dacc53b5a56d631f1c048d5e8b
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-04-02 18:02:33 +0200

    correction on the logo when error occured, added an index on the bottom of the slideshow in desktop version

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -5,12 +5,18 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 export default function Slideshow({ rentalInfo }) {
   const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentNumber, setCurrentNumber] = useState(0)
 
   const nextSlide = () => {
     setCurrentSlide((currentSlideIndex) =>
       currentSlideIndex === rentalInfo.pictures.length - 1
         ? 0
         : currentSlideIndex + 1
+    )
+    setCurrentNumber((currentSildeNumber) =>
+      currentSildeNumber === rentalInfo.pictures.length - 1
+        ? 0
+        : currentSildeNumber + 1
     )
   }
 
@@ -19,6 +25,11 @@ export default function Slideshow({ rentalInfo }) {
       currentSlideIndex === 0
         ? rentalInfo.pictures.length - 1
         : currentSlideIndex - 1
+    )
+    setCurrentNumber((currentSildeNumber) =>
+      currentSildeNumber === 0
+        ? rentalInfo.pictures.length - 1
+        : currentSildeNumber - 1
     )
   }
   return (
@@ -40,6 +51,11 @@ export default function Slideshow({ rentalInfo }) {
           onClick={nextSlide}
           className="arrow arrow__right"
         />
+      )}
+      {rentalInfo.pictures.length > 1 && (
+        <figcaption>
+          {`${currentNumber}/${rentalInfo.pictures.length}`}
+        </figcaption>
       )}
     </div>
   )
